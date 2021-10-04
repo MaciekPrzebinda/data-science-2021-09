ba5be5e69504fee4d8d7123fb830ee48751dfbe5
MaciekPrzebinda <maciej.przebinda@gmail.com>
2021-10-04 18:50:05 +0200

    Initial commit

---
+++ jak masz na imię.py
@@ -0,0 +1,15 @@
+print("Jak masz na imię?")
+name=input()
+print('Ile masz lat?')
+age=input()
+print("Masz na imię " + name + " i masz " + age+ " lat.")
+#druga metoda
+name=input("Jak masz na imię ?")
+age=input("Ile masz lat?")
+print("Masz na imię " + name + " i masz " + age + " lat.")
+
+
+
+
+
+
